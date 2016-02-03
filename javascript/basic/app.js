4aa1a9b79bdcb30f9fe1796f5494e3638b471b9a
Cassio Hubner <cassio@Cassios-MacBook-Pro.local>
2016-02-02 21:06:10 -0600

    Basic concepts

--- javascript/basic/app.js
+++ javascript/basic/app.js
@@ -23,6 +23,34 @@ var w = ++z // increment z, then set w = z
 console.log("z: "+ z)
 console.log("w: "+ w)
 
+/* Strings */
+console.log("\n=== Strings ===");
+
+var string = "Not very long string variable";
+// Lenght => returns the string length
+console.log("Length => "+ string.length);
+console.log("toUpperCase() => "+ string.toUpperCase());
+console.log("toLowerCase() => "+ string.toLowerCase());
+
+// charAt => returns the char at a specified index.
+console.log("charAt(4) => "+ string.charAt(4));
+
+// indexOf() => returns the index of the string parameters first occurance. Return -1 if not found
+console.log("indexOf('long') => "+ string.indexOf('long'));
+//indexOf([fromIndex]) => returns the index of string parameter starting from the specified index
+console.log("indexOf('string',[4]) => "+ string.indexOf('string',[4]));
+
+// substring => returns the char array between two specified index
+console.log("substring(6,11) => "+ string.substring(6,11));
+
+// trim => returns string without lead and tailing spaces
+console.log("trim() => "+ string.trim());
+
+// replace => returns string with replaced characters
+console.log("replace('Not', 'Another') => "+ string.replace('Not', 'Another'));
+
+// split => returns an array of substring of the original string
+console.log("split('r') => "+ string.split("r"));
 
 /* Arrays */
 console.log("\n=== Arrays ===");
@@ -49,12 +77,47 @@ var person1 = {
 	age: 22
 }
 
+console.log(person1)
+
 //construction notation
 var person2 = new Object();
-person['name'] = 'Joao';
-person.age = 25;
+person2['name'] = 'Joao';
+person2.age = 25;
+
+var setAge = function(age){
+	this.age = age; // this this refer the object 
+}
+
+//a function as a object property is called Method
+
+person2.setAge = setAge;
+person2.setAge(99);
+console.log(person2.age);
 
 
+
+
+function Rectangle(height, width) {
+  this.height = height || 8;
+  this.width = width || 9;
+  this.calcArea = function() {
+      return this.height * this.width;
+  };
+  // put our perimeter function here!
+  this.calcPerimeter = function() {
+      return this.height * 2 + this.width * 2;
+  }
+}
+
+var rex = new Rectangle(7,3);
+var puppy = new Rectangle(7); //width = undefined 
+var area = rex.calcArea();
+var perimeter = rex.calcPerimeter();
+
+
+// Ternary Operator
+var grade = 51;
+console.log("You "+ (grade>50 ? 'passed' : 'failed'));
 
 
 
